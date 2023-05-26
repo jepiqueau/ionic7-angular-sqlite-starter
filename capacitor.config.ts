09cee216d68b16780124e97bdc6afa952c771d37
QUEAU Jean Pierre <jepi.queau@free.fr>
2023-05-26 17:29:11 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic7-angular-sqlite-starter',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
